088ddc3df93587a1b68576e72fd7b36e9f1c8b56
Alejandro Guerra <alexguerran2002@gmail.com>
2025-07-28 12:52:03 +0200

    Añadir controlador Solicitud con endpoint GET
    Se ha creado el controlador `SolicitudController` en el archivo `SolicitudController.cs`. Este controlador maneja solicitudes en la ruta `api/solicitud` y proporciona un endpoint GET que devuelve un mensaje de confirmación al ser accedido.

---
+++ PrototipoApiSolucion/PrototipoApi/Controllers/SolicitudController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrototipoApi.Controllers
+{
+    [Route("api/solicitud")]
+    [ApiController]
+    public class SolicitudController : ControllerBase
+    {
+        // Definición de endpoints para el controlador Solicitud
+
+        // GET: api/solicitud
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Solicitud GET endpoint reached");
+        }
+    }
+}
