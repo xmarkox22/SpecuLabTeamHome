4338432589a731e0781420e3f6b8ad9427941263
Jorge-Martinez82 <jmartines@gmail.com>
2025-07-28 13:30:55 +0200

    Añadir clase Building y lógica de manejo de edificios
    Se ha creado un nuevo espacio de nombres `Buildings` con la clase `Building`, que incluye propiedades y métodos para representar y deserializar edificios desde un archivo JSON.
    
    Se ha actualizado `Program.cs` para incluir lógica que crea una lista de edificios ficticios y lee edificios desde `Buildings.json`, imprimiendo la información en la consola.
    
    Además, se ha modificado `SpecuLabConsola.csproj` para añadir una referencia al proyecto `Dominio` y asegurar que `Buildings.json` se copie al directorio de salida.

---
+++ SpecuLab/Dominio/Building.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Buildings;
+
+public class Building
+{
+    public Building(string name = "", int id = 0, int floors = 0, bool hasLift = true, int price = 0)
+    {
+        Name = name;
+        Id = id;
+        Floors = floors;
+        HasLift = hasLift;
+        ApartmentsPerFloor = 4;
+        Price = price;
+    }
+
+    
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public int Floors { get; set; }
+    public bool HasLift { get; set; }
+    public int ApartmentsPerFloor { get; set; }
+    public int Price { get; private set; }
+
+    public Building()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return $"Edificio: {Name}, ID: {Id}, Pisos: {Floors}, Tiene Ascensor: {HasLift}, Apartamentos por piso: {ApartmentsPerFloor}, Precio: {Price}";
+    }
+
+    public static List<Building> FromJsonFile(string v)
+    {
+        var json = File.ReadAllText(v, System.Text.Encoding.UTF8);
+        List<Building> list = JsonSerializer.Deserialize<List<Building>>(json);
+        return list;
+    }
+}
