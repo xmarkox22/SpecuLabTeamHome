e854dff524abfba5f6eb080791c2ee5317688e78
eve <jami.159261@e.unavarra.es>
2025-07-29 14:13:47 +0200

    Leer edificios desde JSON remoto y mejorar salida
    Se ha implementado la funcionalidad para leer edificios desde un archivo JSON remoto. Se han añadido mensajes informativos para imprimir los datos de los edificios leídos y se ha incluido una espera para que el usuario pulse una tecla antes de finalizar el programa.

--- SpecuLab/SpecuLabConsola/Program.cs
+++ SpecuLab/SpecuLabConsola/Program.cs
@@ -29,3 +29,20 @@ foreach (var building in (List<Building>)buildingsFromJson)
     Console.WriteLine(building);
 }
 
+
+// recorre la lista de edificios e imprime sus datos desde el JSON remoto
+Console.WriteLine("================================");
+Console.WriteLine("Leidos desde JSON remoto:");
+var buildingsFromRemoteJson = Building.FromJsonFile("https://api.jsonbin.io/v3/b/68876421ae596e708fbcfb79/latestn");
+foreach (var building in (List<Building>)buildingsFromRemoteJson)
+{
+    Console.WriteLine(building);
+}
+// espera a que el usuario pulse una tecla para finalizar
+Console.WriteLine("================================");
+Console.WriteLine("Pulse una tecla para finalizar...");
+Console.ReadKey();
+
+// fin del programa
+
+
