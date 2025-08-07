6f8213654e629c5469640c1183277d858ebd74fb
Alejandro Guerra <alexguerran2002@gmail.com>
2025-08-07 13:55:51 +0200

    Cambio pequeño en Update del Command

--- PrototipoApiSolucion/PrototipoApi/Application/Requests/Commands/UpdateRequest/UpdateRequestCommand.cs
+++ PrototipoApiSolucion/PrototipoApi/Application/Requests/Commands/UpdateRequest/UpdateRequestCommand.cs
@@ -6,9 +6,7 @@ namespace PrototipoApi.Application.Requests.Commands.UpdateRequest
 {
 
 
-    namespace PrototipoApi.Application.Requests.Commands.UpdateRequest
-    {
-        public record UpdateRequestCommand(int Id, UpdateRequestDto Dto) : IRequest<bool>;
-    }
+ public record UpdateRequestCommand(int Id, UpdateRequestDto Dto) : IRequest<bool>;
+    
 
 }
