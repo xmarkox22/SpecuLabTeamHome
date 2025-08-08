8e57d3c777e6b2d88b55ed9d131ec73ed7ae4d8b
tomas <tomasorejas1@gmail.com>
2025-08-08 13:35:22 +0200

    Status MediaTr

--- PrototipoApiSolucion/PrototipoApi/Controllers/StatusController.cs
+++ PrototipoApiSolucion/PrototipoApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PrototipoApi.BaseDatos;
 using PrototipoApi.Entities;
@@ -13,47 +14,32 @@ namespace PrototipoApi.Controllers
     [ApiController]
     public class StatusController : ControllerBase
     {
-        private readonly ContextoBaseDatos _context;
+        private readonly IMediator _mediator;
 
-        public StatusController(ContextoBaseDatos context)
+        public StatusController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
 
         // GET: api/status
         [HttpGet]
         public async Task<ActionResult<IEnumerable<StatusDto>>> GetStatuses()
         {
-            // Cargamos la lista de estados y proyectamos a DTO
-            var statuses = await _context.Statuses.ToListAsync();
-
-            var dtos = statuses.Select(s => new StatusDto
-            {
-                StatusId = s.StatusId,
-                StatusType = s.StatusType,
-                Description = s.Description
-            }).ToList();
-
-            return Ok(dtos);
+            var statuses = await _mediator.Send(new GetAllStatusQuery());
+            return Ok(statuses);
         }
 
         // GET: api/status/5
         [HttpGet("{id}")]
         public async Task<ActionResult<StatusDto>> GetStatus(int id)
         {
-            var status = await _context.Statuses.FindAsync(id);
+            var status = await _mediator.Send(new GetStatusByIdQuery(id));
             if (status == null)
                 return NotFound();
 
-            var dto = new StatusDto
-            {
-                StatusId = status.StatusId,
-                StatusType = status.StatusType,
-                Description = status.Description
-            };
-
-            return Ok(dto);
+            return Ok(status);
         }
+
 
         // POST: api/status
         [HttpPost]
