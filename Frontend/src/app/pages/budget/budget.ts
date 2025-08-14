308b75f99f0d52591c237e1d781b580ae0ee1461
tomas <tomasorejas1@gmail.com>
2025-08-14 14:26:43 +0200

    PreCasa

--- Frontend/src/app/pages/budget/budget.ts
+++ Frontend/src/app/pages/budget/budget.ts
@@ -20,35 +20,68 @@ export class Budget implements OnInit {
   //Métricas calculadas
   totalBudget = 0;
   countBudgets = 0;
-  avgProfit: number | null = null;
-  pendingCount: number | null = null;
+  avgProfit: number = 10 //| null = null;
+  pendingCount: number = 12 //| null = null;
 
 // Panel de "Comunicación API"
   lastSync: Date | null = null;
-  receivedToday = 0;
+  receivedThisMonth = 0;
 
   // resumen validaciones (si existen estados)
-  approvedCount: number | null = null;
-  rejectedCount: number | null = null;
+  approvedCount: number = 10 //| null = null;
+  rejectedCount: number = 10 //| null = null;
 
   loading = true;
   error = '';
 
   constructor(private budgetService: BudgetService) {}
 
+  // ngOnInit() {
+  //   this.budgetService.getBudgets().subscribe({
+  //     next: (data: ManagementBudget[]) => { // ✅ tipo explícito
+  //       this.budgets = data;
+  //       this.loading = false;
+  //     },
+  //     error: (err: any) => { // ✅ tipo explícito
+  //       console.error(err);
+  //       this.error = 'Error al cargar los presupuestos';
+  //       this.loading = false;
+  //     }
+  //   });
+  // }
+
   ngOnInit() {
+    this.fetchData();
+  }
+
+  private fetchData() {
+    this.loading = false;
+    this.error = '';
+    this.lastSync = new Date();
+    
     this.budgetService.getBudgets().subscribe({
-      next: (data: ManagementBudget[]) => { // ✅ tipo explícito
-        this.budgets = data;
-        this.loading = false;
+      next: (data: ManagementBudget[]) => {
+        this.budgets = Array.isArray(data) ? data : [];
+        this.countBudgets = this.budgets.length;
+
+        // totalBudget
+        this.totalBudget = this.budgets.reduce(
+          (acc, b) => acc + (Number(b.currentAmount) || 0),
+          0
+        );
+
+        // estados
+        
+
+
       },
-      error: (err: any) => { // ✅ tipo explícito
+      error: (err: any) => {
         console.error(err);
         this.error = 'Error al cargar los presupuestos';
+      },
+      complete: () => {
         this.loading = false;
       }
     });
   }
 }
-
-
